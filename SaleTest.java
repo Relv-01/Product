632daac32409acbbd06fca8fffff0df7b338d629
Relv-01 <relvgamer@gmail.com>
2023-07-27 10:23:10 -0500

    Test

---
+++ SaleTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+public class SaleTest {
+    @Test
+    void testCalcDiscount() {
+
+    }
+
+    @Test
+    void testCalcIva() {
+
+    }
+
+    @Test
+    void testGetTotalSale() {
+
+    }
+}
